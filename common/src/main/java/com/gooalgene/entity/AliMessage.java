f413a131151a6a7b80e511489684ac2e01ecddc4
crabime <crabime@gmail.com>
2017-11-02 10:06:25 +0800

    去除无用依赖

--- common/src/main/java/com/gooalgene/entity/AliMessage.java
+++ common/src/main/java/com/gooalgene/entity/AliMessage.java
@@ -6,7 +6,6 @@ import java.util.Map;
 /*
 * ali短信对象，需要传入：username，phone,
 * */
-@Data
 public class AliMessage {
     private User user;
     private String username;
@@ -18,5 +17,51 @@ public class AliMessage {
 
     private String templateCode;
 
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getManagerPhone() {
+        return managerPhone;
+    }
+
+    public void setManagerPhone(String managerPhone) {
+        this.managerPhone = managerPhone;
+    }
+
+    public Map getTemplateParam() {
+        return templateParam;
+    }
+
+    public void setTemplateParam(Map templateParam) {
+        this.templateParam = templateParam;
+    }
+
+    public String getSignName() {
+        return signName;
+    }
+
+    public void setSignName(String signName) {
+        this.signName = signName;
+    }
+
+    public String getTemplateCode() {
+        return templateCode;
+    }
+
+    public void setTemplateCode(String templateCode) {
+        this.templateCode = templateCode;
+    }
 }
