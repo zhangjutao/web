2eacfb7798f5341bef3fb35d37402e0bab040b1e
crabime <crabime@gmail.com>
2017-11-08 17:17:23 +0800

    修复每次进入到首页MyAccessDecisionManager decide方法均调用3次bug 由spring mvc controller首页重定向导致

--- common/src/main/java/com/gooalgene/common/security/MyAccessDecisionManager.java
+++ common/src/main/java/com/gooalgene/common/security/MyAccessDecisionManager.java
@@ -1,17 +1,21 @@
 package com.gooalgene.common.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.FilterInvocation;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.Iterator;
 
 public class MyAccessDecisionManager implements AccessDecisionManager {
-
+    private final static Logger log = LoggerFactory.getLogger(MyAccessDecisionManager.class);
     /**
      * 确定用户是否拥有该权限访问页面
      * @param authentication 执行方法的调用者
@@ -19,6 +23,8 @@ public class MyAccessDecisionManager implements AccessDecisionManager {
      * @param configAttributes 获取该安全对象需要的配置属性
      */
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        FilterInvocation filterInvocation = (FilterInvocation) object;
+        HttpServletRequest request = filterInvocation.getHttpRequest();
         if (configAttributes == null){
             return;
         }
@@ -28,7 +34,8 @@ public class MyAccessDecisionManager implements AccessDecisionManager {
             ConfigAttribute configAttribute = iterator.next();
             //访问所请求资源所需要的权限
             String needPermission = configAttribute.getAttribute();
-            System.out.println("访问"+object.toString()+"需要的权限是：" + needPermission);
+            log.info("访问"+object.toString()+"需要的权限是：" + needPermission);
+            log.info("URL = " + request.getRequestURL());
             //用户所拥有的权限
             Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
             for(GrantedAuthority ga : authorities) {
