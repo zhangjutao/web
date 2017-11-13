02188820a68d00d146458a8a4185c620d0d6956c
吴建业 <245745490@qq.com>
2017-11-13 16:24:31 +0800

    update

--- dna/src/main/java/com/gooalgene/dna/web/DNARunController.java
+++ dna/src/main/java/com/gooalgene/dna/web/DNARunController.java
@@ -11,11 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -101,5 +104,13 @@ public class DNARunController {
             e.printStackTrace();
         }
         return flag;
+    }
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @ResponseBody
+    public List<DNARun> getDunRuns(HttpServletRequest request, DNARun dnaRun,
+                                   @RequestParam(value = "pageNum",defaultValue = "1",required = false) Integer pageNum,
+                                   @RequestParam(value = "pageSize",defaultValue = "10",required = false) Integer pageSize) {
+        return dnaRunService.queryByondition(dnaRun,pageNum,pageSize);
     }
 }
