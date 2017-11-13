5a7e2186f502ab1729b90351aa636558e9708b3f
crabime <crabime@gmail.com>
2017-11-13 21:10:28 +0800

    修复DNARunExample报错问题

--- dna/src/main/java/com/gooalgene/dna/dao/DNARunDao.java
+++ dna/src/main/java/com/gooalgene/dna/dao/DNARunDao.java
@@ -4,7 +4,6 @@ import com.gooalgene.common.CrudDao;
 import com.gooalgene.common.persistence.MyBatisDao;
 import com.gooalgene.dna.dto.DnaRunDto;
 import com.gooalgene.dna.entity.DNARun;
-import com.gooalgene.dna.entity.DnaRunExample;
 
 import java.util.List;
 
@@ -17,8 +16,6 @@ public interface DNARunDao extends CrudDao<DNARun> {
     int insertBatch(List<DNARun> list);
 
     boolean deleteById(int id);
-
-    List<DNARun> selectByExample(DnaRunExample example);
 
     List<DNARun> getListByCondition(DnaRunDto dnaRunDto);
 }
