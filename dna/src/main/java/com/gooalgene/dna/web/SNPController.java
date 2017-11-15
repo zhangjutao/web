cbc1928e5bd696c3ac2a5ae54302525325c6f583
hnz <naizhuanhuang@163.com>
2017-11-15 19:23:46 +0800

    预设接口

--- dna/src/main/java/com/gooalgene/dna/web/SNPController.java
+++ dna/src/main/java/com/gooalgene/dna/web/SNPController.java
@@ -113,8 +113,6 @@ public class SNPController {
     /**
      * 按基因条件搜索
      *
-     * @param request
-     * @param response
      * @return
      */
     @RequestMapping(value = "/condition",method = RequestMethod.GET)
@@ -159,6 +157,16 @@ public class SNPController {
         logger.info("queryBy " + type + " with ctype:" + ctype + ",chr:" + chr + ",startPos:" + startPos + ",endPos:" + endPos + ",group:" + group);
         Page<DNARun> page = new Page<DNARun>(request, response);
         return snpService.searchSNPinRegion(type, ctype, chr, startPos, endPos, group, page);
+    }
+
+    @RequestMapping("/findSampleBySNPId")
+    @ResponseBody
+    public ResultVO genetypePercentBySNPId(HttpServletRequest request, HttpServletResponse response) {
+        String type = request.getParameter("type");
+        String ctype = request.getParameter("ctype");
+        String chr = request.getParameter("chromosome");
+        Map result = snpService.findSampleBySNPId();
+        return ResultUtil.success(result);
     }
 
     /**
