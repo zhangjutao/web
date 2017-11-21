8f7802fba71f89bfebac123a364a53d1f7554f35
吴建业 <245745490@qq.com>
2017-11-21 13:57:50 +0800

    update

--- dna/src/main/java/com/gooalgene/dna/web/SNPController.java
+++ dna/src/main/java/com/gooalgene/dna/web/SNPController.java
@@ -859,7 +859,7 @@ public class SNPController {
         ModelAndView modelAndView=new ModelAndView("snpinfo/snpinfo");
         modelAndView.addObject("snp",snp);
         Map result = snpService.findSampleById(snp.getId());
-        modelAndView.addObject("result",request);
+        modelAndView.addObject("result",result);
         modelAndView.addObject("frequence",frequence);
         return modelAndView;
     }
