6c4bc74446222c0acc5a2be4348d17d881b047ef
crabime <crabime@gmail.com>
2017-11-30 19:53:05 +0800

    增加mybatis从数据库中取出默认空值情况下的设置

--- dna/src/main/java/com/gooalgene/dna/util/StringlToNullHandler.java
+++ dna/src/main/java/com/gooalgene/dna/util/StringlToNullHandler.java
@@ -1,9 +1,6 @@
 package com.gooalgene.dna.util;
 
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
-import org.apache.ibatis.type.MappedJdbcTypes;
-import org.apache.ibatis.type.MappedTypes;
+import org.apache.ibatis.type.*;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
@@ -14,36 +11,25 @@ import java.sql.SQLException;
  * 将mybatis从数据库中查询到的0转化为空字符串
  * @author Crabime
  */
-@MappedJdbcTypes(JdbcType.VARCHAR)
-@MappedTypes(String.class)
-public class StringlToNullHandler extends BaseTypeHandler<String> {
+public class StringlToNullHandler implements TypeHandler<String> {
+
     @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
-        //ps.setDouble(i, Double.parseDouble(parameter)); //将parameter值存入数据库中
-        ps.setString(i,parameter);
+    public void setParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter);
     }
 
     @Override
-    public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
-        //return convertDoubleToString(rs.getDouble(columnName));
-        return convertStringToString(rs.getString(columnName));
+    public String getResult(ResultSet rs, String columnName) throws SQLException {
+        return (rs.getString(columnName) == null)? "-" : rs.getString(columnName);
     }
 
     @Override
-    public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-        return convertStringToString(rs.getString(columnIndex));
+    public String getResult(ResultSet rs, int columnIndex) throws SQLException {
+        return (rs.getString(columnIndex) == null)? "-" : rs.getString(columnIndex);
     }
 
     @Override
-    public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-        return convertStringToString(cs.getString(columnIndex));
-    }
-
-    private String convertStringToString(String value){
-        if (value == ""||value==null){
-            return "-";
-        }else {
-            return String.valueOf(value);
-        }
+    public String getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return (cs.getString(columnIndex) == null)? "-" : cs.getString(columnIndex);
     }
 }
