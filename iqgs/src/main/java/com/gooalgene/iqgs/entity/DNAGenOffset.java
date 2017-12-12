e41c683b02c21916dc3500b5697c4d35b58f07d8
hnz <naizhuanhuang@163.com>
2017-12-12 12:34:35 +0800

    在基因家族的基因信息添加基因name

--- iqgs/src/main/java/com/gooalgene/iqgs/entity/DNAGenOffset.java
+++ iqgs/src/main/java/com/gooalgene/iqgs/entity/DNAGenOffset.java
@@ -3,31 +3,41 @@ package com.gooalgene.iqgs.entity;
 import com.gooalgene.common.DataEntity;
 
 public class DNAGenOffset extends DataEntity<DNAGenOffset> {
+    private String geneName;
+
+    public String getGeneName() {
+        return geneName;
+    }
+
+    public void setGeneName(String geneName) {
+        this.geneName = geneName;
+    }
+
     private String geneId;
     private int length;
-	private int offset;
+    private int offset;
 
-	public String getGeneId() {
-		return geneId;
-	}
+    public String getGeneId() {
+        return geneId;
+    }
 
-	public void setGeneId(String geneId) {
-		this.geneId = geneId;
-	}
+    public void setGeneId(String geneId) {
+        this.geneId = geneId;
+    }
 
-	public int getLength() {
-		return length;
-	}
+    public int getLength() {
+        return length;
+    }
 
-	public void setLength(int length) {
-		this.length = length;
-	}
+    public void setLength(int length) {
+        this.length = length;
+    }
 
-	public int getOffset() {
-		return offset;
-	}
+    public int getOffset() {
+        return offset;
+    }
 
-	public void setOffset(int offset) {
-		this.offset = offset;
-	}
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
 }
