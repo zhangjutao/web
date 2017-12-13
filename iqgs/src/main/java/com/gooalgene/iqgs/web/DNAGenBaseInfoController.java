541a07dd7d80f9a29a15592c30ebae3152e8753a
crabime <songsx@gooalgene.com>
2017-12-13 10:15:36 +0800

    解决TraitCategory问题

--- iqgs/src/main/java/com/gooalgene/iqgs/web/DNAGenBaseInfoController.java
+++ iqgs/src/main/java/com/gooalgene/iqgs/web/DNAGenBaseInfoController.java
@@ -50,6 +50,9 @@ public class DNAGenBaseInfoController {
     @Autowired
     private StudyService studyService;
 
+    @Autowired
+    private QueryService queryService;
+
     /**
      * <span style="color:red;">请求URL</span>: http://host:port/contextPath/iqgs/index <br>
      * 请求方式: GET OR POST
@@ -319,10 +322,6 @@ public class DNAGenBaseInfoController {
         model.addAttribute("genId", genId);
         return "iqgs/IQGS-expression";
     }
-
-    @Autowired
-    private QueryService queryService;
-
 
     @RequestMapping("/detail/qtl")
     public ModelAndView detailForQTL(HttpServletRequest req, HttpServletResponse resp) {
