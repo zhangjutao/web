faffde37e8d8d9b24cd5fc4cb11dc7be1522916d
crabime <songsx@gooalgene.com>
2017-12-15 14:27:23 +0800

    增加微服务框架 增加primer3模块

--- primer3/src/main/java/com/gooalgene/primer/interfaces/impl/DubboServiceImpl.java
+++ primer3/src/main/java/com/gooalgene/primer/interfaces/impl/DubboServiceImpl.java
@@ -3,7 +3,6 @@ package com.gooalgene.primer.interfaces.impl;
 import com.gooalgene.primer.interfaces.DubboService;
 import org.springframework.stereotype.Service;
 
-@Service("dubboService")
 public class DubboServiceImpl implements DubboService {
     public String say() {
         return "hello world";
