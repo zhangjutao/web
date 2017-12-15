4387fb7b80a809698b1f32c410985275f44672fb
danyuliuyan <1415775989@qq.com>
2017-12-15 16:18:07 +0800

    加入了primer3

---
+++ primer3/src/main/java/com/gooalgene/primer/bean/Primer.java
@@ -0,0 +1,102 @@
+package com.gooalgene.primer.bean;
+
+/**
+ * Created by liuyan on 2017/12/15.
+ */
+public class Primer {
+    private int group;
+    private String type;
+    private String position;
+    private String length;
+    private String tm;
+    private String gc;
+    private String any;
+    private String three;
+    private String hairpin;
+    private String sequence;
+    private String link;
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getTm() {
+        return tm;
+    }
+
+    public void setTm(String tm) {
+        this.tm = tm;
+    }
+
+    public String getGc() {
+        return gc;
+    }
+
+    public void setGc(String gc) {
+        this.gc = gc;
+    }
+
+    public String getAny() {
+        return any;
+    }
+
+    public void setAny(String any) {
+        this.any = any;
+    }
+
+    public String getThree() {
+        return three;
+    }
+
+    public void setThree(String three) {
+        this.three = three;
+    }
+
+    public String getHairpin() {
+        return hairpin;
+    }
+
+    public void setHairpin(String hairpin) {
+        this.hairpin = hairpin;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ primer3/src/main/java/com/gooalgene/primer/interfaces/Primer3Service.java
@@ -0,0 +1,13 @@
+package com.gooalgene.primer.interfaces;
+
+import com.gooalgene.primer.bean.Primer;
+
+import java.util.List;
+
+/**
+ * Created by liuyan on 2017/12/15.
+ */
+public interface Primer3Service {
+
+    List<Primer> getPrimer();
+}
