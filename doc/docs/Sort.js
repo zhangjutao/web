14a33c9170c82373d6b355447a7d3daf63a09c6b
crabime <songsx@gooalgene.com>
2018-01-24 10:44:10 +0800

    完善排序api文档

--- doc/docs/Sort.js
+++ doc/docs/Sort.js
@@ -4,16 +4,18 @@ function fetchTraits() {
  * @api {post} /sort/fetch-trait  获取初始性状
  * @apiName fetchTrait
  * @apiGroup Sort
- * @apidescription 排序初始化弹框时，获取性状下拉列表元素
- * @apisamplerequest http://localhost:8080/iqgs/sort/fetch-trait
+ * @apiDescription 排序初始化弹框时，获取性状下拉列表元素
+ * @apiSampleRequest http://localhost:8080/iqgs/sort/fetch-trait
  * @apiSuccessExample Success-Response:
  * [{
  * "id": "14444",
- * "qtlDesc": "无机物耐性"
+ * "qtlDesc": "无机物耐性",
+ * "traitCategoryId": "23"
  * },
  * {
  * "id": "14445",
- * "qtlDesc": "真菌抗性"
+ * "qtlDesc": "真菌抗性",
+ * "traitCategoryId": "16"
  * }
  * ]
  */
@@ -24,8 +26,38 @@ function getSortList() {
  * @api {post} /sort/fetch-sort-list  获取排序结果列表
  * @apiName fetchSortedList
  * @apiGroup Sort
- * @apidescription 排序结果列表
- * @apisamplerequest http://localhost:8080/iqgs/sort/fetch-sort-list
+ * @apiDescription 排序结果列表,注意这里传入的参数。geneExpressionConditionEntities中是一个对象数组，
+ * 用户选择的每一个二级组织需要给它一个不为null的值。
+ * @apiParam {String} traitCategoryId 下拉框选中性状的ID值
+ * @apiParam {String[]} underSortedGeneId 待排序的基因ID数组
+ * @apiParam {Object[]} GeneExpressionConditionEntity 用户选中组织的组织集合，begin、end不传
+ * @apiParam {Number} pageNo 当前页
+ * @apiParam {Number} pageSize 每页大小
+ * @apiParamExample {json} Request-Example:
+ * {
+ *      "msg": "成功",
+ *      "code": 200,
+ *      "data": {
+ *      "traitCategoryId":16,
+ *      "underSortedGeneId": ["Glyma.04G197300", "Glyma.01G182600", "Glyma.02G036200"]
+ *      "geneExpressionConditionEntities":[
+ *      {
+ *          "tissue": {
+ *               "pod":0.0,
+ *               "endosperm": 3.2
+ *          }
+ *      },
+ *      {
+ *          "tissue": {
+ *               "seed":1.0,
+ *               "embryo": 2.2
+ *          }
+ *      }
+ *      ],
+ *      "total": 102
+ *      }
+ * }
+ * @apiSampleRequest http://localhost:8080/iqgs/sort/fetch-sort-list
  * @apiSuccessExample Success-Response:
  * [
  * {
