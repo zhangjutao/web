79db329bb8b1dab174030033b9639eefee1ac116
吴建业 <245745490@qq.com>
2018-01-25 10:18:03 +0800

    fpkm得分方法

--- iqgs/src/test/java/com/gooalgene/iqgs/dao/GeneSortDaoTest.java
+++ iqgs/src/test/java/com/gooalgene/iqgs/dao/GeneSortDaoTest.java
@@ -25,7 +25,7 @@ public class GeneSortDaoTest extends TestCase {
     public void testFindViewByGeneId(){
         String geneId = "Glyma.02G036200";
         String fields="seed,pod";
-        fields=getPrefixOnFields(fields);
+        fields=getPrefixBeforeFields(fields);
         SortedSearchResultView view = geneSortDao.findViewByGeneId(Arrays.asList(new String[]{geneId}),fields,19);
         assertEquals(16.73, view.getFpkm().getPod());
         assertEquals(18.8, view.getFpkm().getShootMeristem());
@@ -36,7 +36,7 @@ public class GeneSortDaoTest extends TestCase {
         assertEquals("3334180bp-3337009bp:-", view.getLocation());
     }
 
-    private String getPrefixOnFields(String fields){
+    private String getPrefixBeforeFields(String fields){
         String[] strings=fields.split(",");
         List<String> strings2= Lists.newArrayList();
         for (String str:strings){
