3ffc1996b8d848c90229cd3ae60911b0968afb3d
吴建业 <245745490@qq.com>
2018-02-09 10:42:28 +0800

    暂时注释aop，报错

--- iqgs/src/main/java/com/gooalgene/iqgs/aspect/ControllerAspect.java
+++ iqgs/src/main/java/com/gooalgene/iqgs/aspect/ControllerAspect.java
@@ -8,6 +8,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.lang.reflect.Method;
 
@@ -16,12 +18,13 @@ import java.lang.reflect.Method;
  * 对整合数据库中Controller接口返回的任何异常导致前台500错误,统一返回
  * 采用AOP机制
  */
-@Aspect
+//@Aspect
+//@Controller
 public class ControllerAspect {
     private final static Logger logger = LoggerFactory.getLogger(ControllerAspect.class);
 
-    @Around("execution(* com.gooalgene.iqgs.web.*.*(..)) && @annotation(org.springframework.web.bind.annotation.ResponseBody)")
-    public Object catchException(ProceedingJoinPoint call){
+    //@Around("execution(* com.gooalgene.iqgs.web.*.*(..)) && @annotation(org.springframework.web.bind.annotation.ResponseBody)")
+    /*public Object catchException(ProceedingJoinPoint call){
         MethodSignature signature = (MethodSignature) call.getSignature();
         Method method = signature.getMethod();
         Object[] args = call.getArgs();
@@ -33,5 +36,5 @@ public class ControllerAspect {
             result = ResultUtil.error(500, "服务器出错");
         }
         return result;
-    }
+    }*/
 }
