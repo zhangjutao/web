453271a4c06d50653abffce7e2e4d3f3e7dae579
crabime <songsx@gooalgene.com>
2018-02-24 14:32:58 +0800

    修复QTL高级搜索与一级搜索不一致问题

--- iqgs/src/main/java/com/gooalgene/iqgs/web/DNAGenBaseInfoController.java
+++ iqgs/src/main/java/com/gooalgene/iqgs/web/DNAGenBaseInfoController.java
@@ -35,7 +35,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
