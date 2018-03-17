3e7fb017f365af4442632a86ba4525c5b632097c
Archer <15107152937@163,com>
2018-03-17 17:00:25 +0800

    dna接口参数格式修改

--- dna/src/main/webapp/static/js/addTags.js
+++ dna/src/main/webapp/static/js/addTags.js
@@ -350,6 +350,7 @@ $(function (){
                     //舍弃小数之后的取整
                     intNums = parseInt(count / page.pageSize);
                     for (var i=0;i<totalDatas.length;i++){
+                        var idTV = totalDatas[i].id==null?"":totalDatas[i].id;  // 测序样品编号
                         var runNoTV = totalDatas[i].runNo==null?"":totalDatas[i].runNo;  // 测序样品编号
                         var scientificNameTV = totalDatas[i].scientificName==null?"":totalDatas[i].scientificName;  // 物种名称
                         var sampleIdTV = totalDatas[i].sampleId==null?"":totalDatas[i].sampleId;  // 编号
@@ -391,7 +392,8 @@ $(function (){
                         var matingTypeTV = totalDatas[i].matingType==null?"":totalDatas[i].matingType;  //交配型
                         var tr = "<tr>" +
                             "<td class='paramTag'><input type='checkbox'/></td>" +
-                            "<td class='paramTag runNoTV'>" + runNoTV+
+                            "<td class='paramTag runNoTV'>" + idTV+
+                            "</td><td class='paramTag runNoTV'>" + runNoTV+
                             "</td><td class='paramTag scientificNameTV'>" + scientificNameTV+
                             "</td><td class='paramTag sampleIdTV'>" + sampleIdTV+
                             "</td><td class='paramTag strainNameTV'>" + strainNameTV +
