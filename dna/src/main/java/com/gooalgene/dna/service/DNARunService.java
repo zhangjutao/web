2777de066cef0f81379bfe2a9ea1613270b8dc01
crabime <songsx@gooalgene.com>
2018-03-17 16:55:35 +0800

    修改选择多个品种组成群体时group与idList之间接口

--- dna/src/main/java/com/gooalgene/dna/service/DNARunService.java
+++ dna/src/main/java/com/gooalgene/dna/service/DNARunService.java
@@ -5,13 +5,13 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.gooalgene.common.Page;
 import com.gooalgene.dna.dao.DNARunDao;
-import com.gooalgene.dna.dto.DnaRunDto;
 import com.gooalgene.dna.dto.SampleInfoDto;
 import com.gooalgene.dna.entity.DNARun;
 import com.gooalgene.dna.entity.SampleInfo;
-import com.gooalgene.dna.entity.result.DNARunSearchResult;
 import com.gooalgene.dna.entity.result.GroupCondition;
 import com.gooalgene.dna.util.JacksonUtils;
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -42,25 +42,27 @@ public class DNARunService {
     private ObjectMapper objectMapper = new ObjectMapper();
     /**
      * 根据页面分组查询对应的样本信息
+     * 在选择品种查询时，传入的一个group对应多个品种，前端传入的品种均为ID值，改ID集合存放在condition变量中，key为idList
+     * 返回一个Map：key(群体名称), value(多个idList)
      */
-    public Map<String, List<String>> queryDNARunByCondition(String group) throws IOException {
-        //List<GroupCondition> result = objectMapper.readValue(group, new TypeReference<List<GroupCondition>>() {});
-        GroupCondition entity = objectMapper.readValue(group, GroupCondition.class);
+    public Map<String, List<String>> queryDNARunByCondition(String group) {
+        Map<String, List<String>> result = new HashMap<>();
         try {
             List<GroupCondition> groupConditions = JacksonUtils.convertJsonToArray(group, GroupCondition.class);
-            GroupCondition condition = null;
-            for (int i = 0; i < groupConditions.size(); i++) {
-                condition = groupConditions.get(i);
-                String groupName = condition.getName();
-                Map<String, Object> parameters = condition.getCondition();
-                Set<String> keySet = parameters.keySet();
-
+            for (GroupCondition input : groupConditions) {
+                String groupName = input.getName();
+                List<String> finalIdList = new ArrayList<>();
+                String idList = (String) input.getCondition().get("idList");
+                // 如果传入的id集合collection属性中包含idList字段且值包含多个sample_info ID值
+                if (!org.springframework.util.StringUtils.isEmpty(idList) && idList.contains(",")) {
+                    finalIdList = Arrays.asList(idList.split(","));
+                }
+                result.put(groupName, finalIdList);
             }
         } catch (IOException e) {
             logger.error("传入JSON字符串：" + group + "异常", e.getCause());
             e.printStackTrace();
         }
-        Map<String, List<String>> result = new HashMap<>();
         return result;
     }
 
