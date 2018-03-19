aa7d6895ee745f61705a197416539adda14faaa1
hnz <naizhuanhuang@163.com>
2018-03-19 14:06:40 +0800

    导出数据

--- dna/src/main/java/com/gooalgene/dna/dto/SampleInfoDto.java
+++ dna/src/main/java/com/gooalgene/dna/dto/SampleInfoDto.java
@@ -1,7 +1,10 @@
 package com.gooalgene.dna.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SampleInfoDto {
 
     private String id;
